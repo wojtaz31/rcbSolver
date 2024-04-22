b581356ef4c3187ea6697cb4c47f939a2293473a
wojtaz31 <wojtaz31@wp.pl>
2024-04-22 22:45:06 +0200

    alpha version of scritpt - works perfectly fine

---
+++ rcbSolver.js
@@ -0,0 +1,22 @@
+function waitFor(delay) {
+    return new Promise(resolve => { setTimeout(resolve, delay) })
+}
+
+let inps = true
+
+while (inps) {
+    let inputBody = document.querySelector('.render').shadowRoot
+    .querySelector('#lw-90').querySelector('#lw-72').querySelector('#lw-97')
+    .querySelector('#lw-103').querySelector('#lw-76').shadowRoot
+    
+    let submitBody = document.querySelector('.render').shadowRoot
+    .querySelector('#lw-90').querySelector('#lw-72').querySelector('#lw-97')
+    .querySelector('#lw-103').querySelector('#lw-75').shadowRoot
+
+    inps = Array.from(inputBody.querySelectorAll('input'))
+    let i = Math.floor(Math.random() * 3)
+    inps[i].click()
+    inps[i].checked = true
+    submitBody.querySelector('button').click()
+    await waitFor(500)
+}
